cd37a73fe52d37d97e2bcf43e097ecd67a1e4436
gsharma <gsharma@adobe.com>
2017-02-25 00:35:01 +0530

    Optimized Threadpool.
    compile using g++ -std=c++11 -o tp -pthread main.cpp

--- threadpool/threadpool/Threadpool.hpp
+++ threadpool/threadpool/Threadpool.hpp
@@ -12,21 +12,106 @@
 #include <thread>
 #include <vector>
 #include <map>
+#include <mutex>
+#include <unistd.h>
+#include <iostream>
+using namespace std;
 
-
+template <typename Key, typename Value>
 class Threadpool
 {
 	std::vector<std::thread> threads;
-	std::map<int, int> result;
+	std::map<Key, Value> result;
 	std::mutex m;
-	int toPrint;
+	Key toPrint;
+	void threadFunc();
 public:
 	Threadpool();
-	virtual ~Threadpool();
+	virtual ~Threadpool() {}
 	void complete(int result);
 	void print();
 	void endWork();
 	bool end;
 };
+
+template <typename Key>
+Key getWork()
+{
+	static std::mutex cMutex;
+	static Key counter = 0;
+	Key retVal = 0;
+	cMutex.lock();
+	retVal = counter;
+	counter += 1;
+	cMutex.unlock();
+	return retVal;
+}
+
+template <typename Key, typename Value>
+void Threadpool<Key, Value>::complete(int r)
+{
+	m.lock();
+	result[r] = r;
+	m.unlock();
+	print();
+}
+
+template <typename Key, typename Value>
+void Threadpool<Key, Value>::threadFunc()
+{
+	while(!end)
+	{
+		int work = getWork<Key>();
+		for(int j = 0; j < 10; j++)
+		{
+			usleep(rand() * 100000.0/ RAND_MAX);
+		}
+
+		complete(work + 1);
+	}
+}
+
+template <typename Key, typename Value>
+void Threadpool<Key, Value>::print()
+{
+	m.lock();
+	while(result.find(toPrint) != result.end() && !result.empty())
+	{
+		cout<<toPrint<<endl;
+		result.erase(toPrint);
+		toPrint++;
+	}
+	m.unlock();
+}
+
+template <typename Key, typename Value>
+void Threadpool<Key, Value>::endWork()
+{
+	end = true;
+
+	for(size_t i = 0; i < threads.size();i++)
+	{
+		if(threads[i].joinable())
+			threads[i].join();
+	}
+
+	while(!result.empty())
+	{
+		print();
+	}
+}
+
+template <typename Key, typename Value>
+Threadpool<Key, Value>::Threadpool() : end(false), toPrint(1)
+{
+	srand(0);
+	unsigned numThreads = std::thread::hardware_concurrency();
+	threads.reserve(numThreads);
+	for(unsigned i = 0; i < numThreads; i++)
+	{
+		threads.push_back(std::thread(&Threadpool<Key, Value>::threadFunc, this));
+	}
+}
+
 
 #endif /* Threadpool_h */
